b564488fe4fbaf609bfae20b899ae1d465148216
Emil <emillau@nine-yi.com>
2024-04-29 15:56:25 +0800

    feat: seed stock revenue table

--- database.ts
+++ database.ts
@@ -1,8 +1,10 @@
 import mysql from "mysql2";
 import {
   STOCK_BASIC_INFO_TABLE,
+  STOCK_REVENUE_TABLE,
   dbName,
   placeholderForBasicInfo,
+  placeholderForStockRevenue,
 } from "./constant";
 import dotenv from "dotenv";
 import { query } from "./query";
@@ -26,26 +28,48 @@ const isTableEmpty = async (table: string): Promise<boolean> => {
 
 const seedStockInfo = async (): Promise<void> => {
   await Promise.all(
-    placeholderForBasicInfo.map((item) =>
-      pool.execute(query.INSERT_STOCK_INFO, [
+    placeholderForBasicInfo.map(async(item) =>
+      {
+        await pool.query(`USE ${dbName}`);
+        pool.execute(query.INSERT_STOCK_INFO, [
         item.industry_category,
         item.stock_id,
         item.stock_name,
         item.type,
         item.date,
-      ])
+      ])}
     )
   );
 };
 
-const initDB = async () => {
+const seedStockRevenue = async (): Promise<void> => {
+  await Promise.all(
+    placeholderForStockRevenue.map(async (item) => {
+      try {
+        // TOFIX: Why seeding only success if I use database in every iteration?
+        await pool.query(`USE ${dbName}`);
+        await pool.execute(query.INSERT_STOCK_REVENUE, [
+          item.date,
+          item.stock_id,
+          item.country,
+          item.revenue,
+          item.revenue_month,
+          item.revenue_year,
+        ]);
+      } catch (error) {
+        console.error("Error inserting item:", item, error);
+      }
+    })
+  );
+};
+const initDB = async (): Promise<void> => {
   try {
     await pool.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`);
     await pool.query(`USE ${dbName}`);
     await pool.query(query.CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST);
-    if (await isTableEmpty(STOCK_BASIC_INFO_TABLE)) {
-      await seedStockInfo();
-    }
+    await pool.query(query.CREATE_STOCK_REVENUE_IF_NOT_EXIST);
+    if (await isTableEmpty(STOCK_BASIC_INFO_TABLE)) await seedStockInfo();
+    if (await isTableEmpty(STOCK_REVENUE_TABLE)) await seedStockRevenue();
   } catch (error) {
     console.log("this is error", error);
   }
--- query.ts
+++ query.ts
@@ -8,12 +8,18 @@ const CREATE_STOCK_REVENUE_IF_NOT_EXIST = await fs.readFile(
   { encoding: "utf8" }
 );
 const INSERT_STOCK_INFO = await fs.readFile(
-    "./query/basicInfo/INSERT_STOCK_INFO.sql",
-    { encoding: "utf8" }
-  );
+  "./query/basicInfo/INSERT_STOCK_INFO.sql",
+  { encoding: "utf8" }
+);
+
+const INSERT_STOCK_REVENUE = await fs.readFile(
+  "./query/stockRevenue/INSERT_STOCK_REVENUE.sql",
+  { encoding: "utf8" }
+);
 
 export const query = {
   CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST,
   CREATE_STOCK_REVENUE_IF_NOT_EXIST,
-  INSERT_STOCK_INFO
+  INSERT_STOCK_INFO,
+  INSERT_STOCK_REVENUE
 };
