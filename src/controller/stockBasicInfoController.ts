7edb6797200d0e9fff1a46a8211b4f9c3c66079c
Emil <emillau@nine-yi.com>
2024-04-30 13:53:44 +0800

    feat: seperate api endpoint for basicInfo & stockRevenue

--- src/controller/stockBasicInfoController.ts
+++ src/controller/stockBasicInfoController.ts
@@ -1,60 +1,70 @@
-import { Request, Response } from 'express';
-import { FirmmindDataTypeEnum, HttpStatus } from '../constant';
-import { getSpecificStockInfo, getAllStockInfo } from './database';
-import { ResponseClass } from '../domain/response';
+import { Request, Response } from "express";
+import { FirmmindDataTypeEnum, HttpStatus } from "../constant";
+import { getSpecificStockInfo, getAllStockInfo } from "./database";
+import { ResponseClass } from "../domain/response";
 
 // TOASK: it will cluster all logic here, how to avoid?
-export const getBasicInfo = async(req:Request,res:Response) =>{
-    const { dataset, dataId } = req.query;
-  
-    if (dataset === FirmmindDataTypeEnum.TaiwanStockInfo) {
-      try {
-        if (dataId) {
-          const specificStockInfo = await getSpecificStockInfo(Number(dataId));
-          if (specificStockInfo.length) {
-            return res.send(
-              new ResponseClass(
-                HttpStatus.OK.status,
-                HttpStatus.OK.code,
-                specificStockInfo
-              )
-            );
-          } else {
-            return res.send(
-              new ResponseClass(
-                HttpStatus.NO_CONTENT.status,
-                HttpStatus.NO_CONTENT.code
-              )
-            );
-          }
-        } else {
-          const allStockInfo = await getAllStockInfo();
-          if (allStockInfo.length) {
-            return res.send(
-              new ResponseClass(HttpStatus.OK.status, HttpStatus.OK.code, allStockInfo)
-            );
-          } else {
-            return res.send(
-              new ResponseClass(
-                HttpStatus.NO_CONTENT.status,
-                HttpStatus.NO_CONTENT.code,
-                []
-              )
-            );
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching stock info:", error);
-        return res
-          .status(500)
-          .send(
+export const getBasicInfo = async (req: Request, res: Response) => {
+  const { dataset, dataId } = req.query;
+
+  if (dataset === FirmmindDataTypeEnum.TaiwanStockInfo) {
+    try {
+      if (dataId) {
+        const specificStockInfo = await getSpecificStockInfo(Number(dataId));
+        if (specificStockInfo.length) {
+          return res.send(
             new ResponseClass(
-              HttpStatus.INTERNAL_SERVER_ERROR.status,
-              HttpStatus.INTERNAL_SERVER_ERROR.code
+              HttpStatus.OK.status,
+              HttpStatus.OK.code,
+              specificStockInfo
             )
           );
+        }
+        return res.send(
+          new ResponseClass(
+            HttpStatus.NO_CONTENT.status,
+            HttpStatus.NO_CONTENT.code
+          )
+        );
+      } else {
+        //  no specific dataId, get all stock
+        const allStockInfo = await getAllStockInfo();
+        if (allStockInfo.length) {
+          return res.send(
+            new ResponseClass(
+              HttpStatus.OK.status,
+              HttpStatus.OK.code,
+              allStockInfo
+            )
+          );
+        } else {
+          return res.send(
+            new ResponseClass(
+              HttpStatus.NO_CONTENT.status,
+              HttpStatus.NO_CONTENT.code
+            )
+          );
+        }
       }
-    } else {
-      return res.status(404).send(new ResponseClass("not_found", 404, []));
+    } catch (error) {
+      console.error("Error fetching stock info:", error);
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+        .send(
+          new ResponseClass(
+            HttpStatus.INTERNAL_SERVER_ERROR.status,
+            HttpStatus.INTERNAL_SERVER_ERROR.code
+          )
+        );
     }
+  } else {
+    return res
+      .status(HttpStatus.NOT_FOUND.code)
+      .send(
+        new ResponseClass(
+          HttpStatus.NOT_FOUND.status,
+          HttpStatus.NOT_FOUND.code
+        )
+      );
   }
+};
--- src/route/stock.route.ts
+++ src/route/stock.route.ts
@@ -2,6 +2,6 @@ import express from "express";
 import { getBasicInfo } from "../controller/stockBasicInfoController";
 
 const stockRoute = express.Router();
-stockRoute.get("/api/data", getBasicInfo)
+stockRoute.get("/api/basicInfo", getBasicInfo)
 
 export default stockRoute;
