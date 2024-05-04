9c85dbaae11c056f4d516faa3497a9d0b8f29d84
Emil <emillau@nine-yi.com>
2024-05-04 22:05:58 +0800

    feat: use dirname to serve static file

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import { initDB } from "./controller/database";
 import stockRoute from "./route/stock.route";
 import { HttpStatus } from "./constant";
 import { ResponseClass } from "./domain/response";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 
 const app = express();
 const port = 3002;
-app.use(express.static("./public"))
+app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 
 // TOASK: Why app.use is necessary
@@ -31,6 +37,6 @@ app.get("/", (req, res) =>
   res.send(new ResponseClass("API running, please fetch specific endpoint", HttpStatus.OK.code))
 );
 
-app.get("*", (req, res) =>
-  res.send(new ResponseClass("API running, this is not valid endpoint", HttpStatus.OK.code))
-);
+// app.get("*", (req, res) =>
+//   res.send(new ResponseClass("API running, this is not valid endpoint", HttpStatus.OK.code))
+// );
