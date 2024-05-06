1fa6f41298a01f782bb20c9acabea91c358e6b4c
Emil <emillau@nine-yi.com>
2024-05-06 15:05:49 +0800

    feat: revert changes for query location

--- src/query.ts
+++ src/query.ts
@@ -1,30 +1,26 @@
 import fs from "fs/promises";
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const basePath = path.resolve(__dirname, "../query");
+const CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST = await fs.readFile(
+  "./src/query/basicInfo/CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST.sql",
+  { encoding: "utf8" }
+);
+const CREATE_STOCK_REVENUE_IF_NOT_EXIST = await fs.readFile(
+  "./src/query/stockRevenue/CREATE_STOCK_REVENUE_IF_NOT_EXIST.sql",
+  { encoding: "utf8" }
+);
+const INSERT_STOCK_INFO = await fs.readFile(
+  "./src/query/basicInfo/INSERT_STOCK_INFO.sql",
+  { encoding: "utf8" }
+);
 
-const readFile = async (filePath:string) => {
-  try {
-    return await fs.readFile(filePath, { encoding: "utf8" });
-  } catch (error) {
-    console.error(`Error reading file ${filePath}:`, error);
-    return null;
-  }
-};
+const INSERT_STOCK_REVENUE = await fs.readFile(
+  "./src/query/stockRevenue/INSERT_STOCK_REVENUE.sql",
+  { encoding: "utf8" }
+);
 
 export const query = {
-  CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST: await readFile(
-    path.join(basePath, "basicInfo", "CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST.sql")
-  ),
-  CREATE_STOCK_REVENUE_IF_NOT_EXIST: await readFile(
-    path.join(basePath, "stockRevenue", "CREATE_STOCK_REVENUE_IF_NOT_EXIST.sql")
-  ),
-  INSERT_STOCK_INFO: await readFile(
-    path.join(basePath, "basicInfo", "INSERT_STOCK_INFO.sql")
-  ),
-  INSERT_STOCK_REVENUE: await readFile(
-    path.join(basePath, "stockRevenue", "INSERT_STOCK_REVENUE.sql")
-  ),
+  CREATE_STOCK_BASIC_INFO_IF_NOT_EXIST,
+  CREATE_STOCK_REVENUE_IF_NOT_EXIST,
+  INSERT_STOCK_INFO,
+  INSERT_STOCK_REVENUE
 };
